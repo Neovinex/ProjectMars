03b3d7c37bba3246e5e10dd78c95dcdd2b2b6223
Neo <neowij@yahoo.com>
2023-06-23 16:12:43 +1000

    Steps for Features Pages Tests Utitlities created

---
+++ ProjectMars/ProjectMars/Utilities/CommonDriver.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ProjectMars.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMars.Utilities
+{
+    public class CommonDriver
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void LoginSteps()
+        {
+            driver = new ChromeDriver();
+            //Login Page object initialization and definition
+            LoginPage LoginPageObj = new LoginPage();
+            LoginPageObj.LoginSteps(driver);
+
+        }
+
+        [OneTimeTearDown]
+        public void ClosingSteps()
+        {
+            driver.Quit();
+        }
+    }
+}
